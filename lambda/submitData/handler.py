d10e8b093fd03c9fe54f37a3adf6124d094f70d7
Aarash Samimi <aarash.samimi@cdw.com>
2025-04-25 10:50:59 -0700

    fixed lambda handler

--- lambda/submitData/handler.py
+++ lambda/submitData/handler.py
@@ -2,15 +2,22 @@ import json
 import boto3
 import os
 
-#SubmitData Lambda function to send messages to SQS queue
+# SubmitData Lambda function to send messages to SQS queue
 sqs = boto3.client('sqs')
+s3 = boto3.client('s3')  # To interact with S3
 QUEUE_URL = os.environ.get('QUEUE_URL')
+BUCKET_NAME = os.environ.get('BUCKET_NAME')  # Get the S3 bucket name from environment variables
 
 def lambda_handler(event, context):
     try:
         body = json.loads(event['body']) if 'body' in event else event
         message = json.dumps(body)
 
+        # Example of using BUCKET_NAME (e.g., upload a message to the S3 bucket)
+        if BUCKET_NAME:
+            s3.put_object(Bucket=BUCKET_NAME, Key="message.json", Body=message)  # Uploading the message to S3
+
+        # Send message to SQS
         response = sqs.send_message(
             QueueUrl=QUEUE_URL,
             MessageBody=message
