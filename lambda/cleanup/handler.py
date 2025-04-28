678791de20d4f07f4ca2cee49caf26dd4381ba07
Aarash Samimi <aarash.samimi@cdw.com>
2025-04-28 12:09:16 -0700

    updated cleanup function to only delete uploaded images

--- lambda/cleanup/handler.py
+++ lambda/cleanup/handler.py
@@ -2,20 +2,26 @@ import boto3
 import datetime
 import os
 
-#cleanup Lambda function to delete old objects from S3 bucket
+# Initialize the S3 client
 s3 = boto3.client('s3')
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
 MAX_AGE_DAYS = 7
+UPLOADS_PREFIX = 'uploads/'  # Only clean files inside uploads/
 
 def lambda_handler(event, context):
     deleted = []
     threshold = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=MAX_AGE_DAYS)
-    objects = s3.list_objects_v2(Bucket=BUCKET_NAME).get('Contents', [])
-
-    for obj in objects:
-        if obj['LastModified'] < threshold:
-            s3.delete_object(Bucket=BUCKET_NAME, Key=obj['Key'])
-            deleted.append(obj['Key'])
+    
+    # List objects only under uploads/
+    paginator = s3.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(Bucket=BUCKET_NAME, Prefix=UPLOADS_PREFIX)
+    
+    for page in page_iterator:
+        objects = page.get('Contents', [])
+        for obj in objects:
+            if obj['LastModified'] < threshold:
+                s3.delete_object(Bucket=BUCKET_NAME, Key=obj['Key'])
+                deleted.append(obj['Key'])
 
     return {
         "statusCode": 200,
