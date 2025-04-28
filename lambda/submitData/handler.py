9a1629cb0819d5fdb18f6a605cdf27c622dff57d
Aarash Samimi <aarash.samimi@cdw.com>
2025-04-28 15:59:43 -0700

    lambda fix

--- lambda/submitData/handler.py
+++ lambda/submitData/handler.py
@@ -16,17 +16,20 @@ def lambda_handler(event, context):
         body = event.get('body')
         if event.get('isBase64Encoded'):
             body = base64.b64decode(body)
+        else:
+            body = body.encode('utf-8')
 
-        # Parse JSON if necessary
-        body_json = json.loads(body)
+        # Now decode JSON from the body
+        data = json.loads(body)
 
-        # Get the file name and file data
-        file_name = body_json['file_name']
-        file_data = base64.b64decode(body_json['file_data'])
+        # Get filename and file_data from the body
+        file_name = data['file_name']
+        file_data = base64.b64decode(data['file_data'])
 
+        # Define where to upload (dynamic filename)
         file_key = f"uploads/{file_name}"
 
-        # Upload the file to S3
+        # Upload the decoded file to S3
         s3.put_object(Bucket=BUCKET_NAME, Key=file_key, Body=file_data)
 
         # Send a message to SQS
@@ -39,7 +42,7 @@ def lambda_handler(event, context):
                 "Access-Control-Allow-Methods": "POST, OPTIONS",
                 "Access-Control-Allow-Headers": "Content-Type, Authorization",
             },
-            "body": json.dumps({"message": "Upload successful", "fileKey": file_key})
+            "body": json.dumps({"message": "Upload successful"})
         }
 
     except Exception as e:
